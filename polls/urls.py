3435d0bbe6d01eb19443b0f9c6a535e071a7e5d2
Tạ Quang Ngọc <taquangngoc.hh3@gmail.com>
2021-09-15 23:34:10 +0700

    part 3: create basic view using django api

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name = 'index')
+    # example /polls/
+    path('', views.index, name = 'index'),
+
+    # example /polls/5
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # example /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # example /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
 
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Question 
+
 def index(request):
-    return HttpResponse("Đây là response haahaaha!!!")
+    latest_question_list = Question.objects.order_by('pub_date')[:]
+    # create context for html template
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
 
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
+
+def results(request, question_id):
+    response = "Bạn đang xem kết quả của câu hỏi %s"
+    return HttpResponse(response % question_id)   
+
+def vote(request, question_id):
+    return HttpResponse("Bạn đang thực hiện vote cho câu hỏi %s" % question_id)
